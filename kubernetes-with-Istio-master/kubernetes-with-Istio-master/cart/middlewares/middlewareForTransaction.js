f7813f904acf322af3c53a3f4a5559d36e859043
jognhan <160691663+jognhan@users.noreply.github.com>
2024-03-11 16:43:46 +0900

    Delete kubernetes-with-Istio-master/kubernetes-with-Istio-master directory

--- kubernetes-with-Istio-master/kubernetes-with-Istio-master/cart/middlewares/middlewareForTransaction.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-    next();
-    for(let key of Object.keys(req.headers)){
-        if(key.startsWith('x-')){
-            res.header[key] = req.headers[key];
-        }
-    }
-}
